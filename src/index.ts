7bed00c6e9597c34b2ac11cd3c20099bec99e378
Liqueur <Liiiiiiiqueur.gmail.com>
2023-12-25 23:48:18 +0900

    fourth commit

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,9 @@ myTodoCollection.addTodo("친구 만나기");
 myTodoCollection.markComplete(3, true);
 
 console.log(`${myTodoCollection.userName}`);
-myTodoCollection.todoItems.forEach((item) => item.printDetails());
+
+myTodoCollection.removeComplete();
+
+myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+console.log("=============================");
+myTodoCollection.getTodoItems(false).forEach((item) => item.printDetails());
